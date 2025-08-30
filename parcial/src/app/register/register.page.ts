c1ec3660574403338d58d31a24d9025492e2134b
Loldeluis <luismaldonadourib@gmail.com>
2025-08-30 18:08:44 -0500

    register funcional unicamente con usuario y contraseña

--- parcial/src/app/register/register.page.ts
+++ parcial/src/app/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component} from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -7,8 +9,40 @@ import { Component} from '@angular/core';
   standalone:  false
 })
 export class RegisterPage {
+ username: string = '';
+  password: string = '';
 
-  constructor() { }
+  constructor(private router: Router, private alertCtrl: AlertController) { }
+
+   async register() {
+    if (!this.username || !this.password) {
+      this.showAlert('Error', 'Todos los campos son obligatorios');
+      return;
+    }
+
+    if (localStorage.getItem(this.username)) {
+      this.showAlert('Error', 'El usuario ya existe');
+      return;
+    }
+
+      const userData = {
+      username: this.username,
+      password: this.password,
+    };
+
+     localStorage.setItem(this.username, JSON.stringify(userData));
+    this.showAlert('Éxito', 'Usuario registrado correctamente');
+    this.router.navigate(['/home']);
+  }
+
+   async showAlert(header: string, message: string) {
+    const alert = await this.alertCtrl.create({
+      header,
+      message,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
 
 
 }
