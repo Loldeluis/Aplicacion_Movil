aa97197165e51659df093691e7debbf5bb04922d
Loldeluis <luismaldonadourib@gmail.com>
2025-08-31 01:27:10 -0500

    integracion de API en bruto, sin organizacion

--- parcial/src/app/dashboard/dashboard.page.ts
+++ parcial/src/app/dashboard/dashboard.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../shared/auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,9 +9,30 @@ import { AuthService } from '../shared/auth.service';
   standalone: false
 })
 export class DashboardPage implements OnInit {
-  constructor(private authService: AuthService) { }
+  news: any[] = [];
+  loading = true;
+  error = '';
 
-  ngOnInit() {}
+  constructor(private authService: AuthService, private http: HttpClient) { }
+
+  ngOnInit() {
+    this.fetchNews();
+  }
+
+  fetchNews() {
+    this.loading = true;
+    this.http.get<any>('https://newsapi.org/v2/everything?q=tesla&from=2025-07-31&sortBy=publishedAt&apiKey=4e933b43792842b6be7c199ab7a2352d')
+      .subscribe({
+        next: res => {
+          this.news = res.articles || [];
+          this.loading = false;
+        },
+        error: err => {
+          this.error = 'No se pudieron cargar las noticias.';
+          this.loading = false;
+        }
+      });
+  }
 
   logout() {
     this.authService.logout();
